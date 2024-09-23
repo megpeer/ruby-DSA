a726b9ac68c23d3a0cefee8e35178329417234b9
megpeer <148973963+megpeer@users.noreply.github.com>
2024-09-23 09:39:52 -0400

    folder structure sorted to separate projects, hashmap project framework ready for learning.

---
+++ hashmap/hashmap.rb
@@ -0,0 +1,60 @@
+#snippet to use whenever accessing buckets:
+#  raise IndexError if index.negative? || index >= @buckets.length
+
+
+class HashMap
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+       
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+       
+    hash_code
+  end
+
+  # takes two arguments, the first is a key and the second is a value that is assigned to this key. If a key already exists, then the old value is overwritten or we can say that we update the key’s value
+  def set(key, value)
+  
+  end
+
+  # takes one argument as a key and returns the value that is assigned to this key. If key is not found, return nil.
+  def get(key)
+ 
+  end
+
+  # takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
+  def has?(key)
+    
+  end
+
+  # takes a key as an argument. If the given key is in the hash map, it should remove the entry with that key and return the deleted entry’s value. If the key isn’t in the hash map, it should return nil.
+  def remove(key)
+    
+  end
+
+  # returns the number of stored keys in the hash map.
+  def length
+    
+  end
+
+  # removes all entries in the hash map.
+  def clear
+    
+  end
+
+  # returns an array containing all the keys inside the hash map.
+  def keys
+    
+  end
+
+  # returns an array containing all the values.
+  def values
+
+  end
+
+  # returns an array that contains each key, value pair. Example: [[first_key, first_value], [second_key, second_value]]
+  def entries
+    
+  end
+end
