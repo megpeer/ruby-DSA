f28d8ed0153e88e6dbe1670a9694ff353d780a56
megpeer <148973963+megpeer@users.noreply.github.com>
2024-09-30 10:39:47 -0400

    all specified functionality complete besides linked list entries

--- hashmap/hashmap.rb
+++ hashmap/hashmap.rb
@@ -24,23 +24,19 @@ class HashMap
        
     key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
     hash_code = hash_code % bucket.size
-    puts "hash code for #{key} is #{hash_code}"
     return hash_code
     
   end
 
-  # takes two arguments, the first is a key and the second is a value that is assigned to this key. If a key already exists, then the old value is overwritten or we can say that we update the key’s value
+  #adds a key value pair ot the hashmap
   def set(key, value)
-    
+    # length
+    # load_factor = @size
     index = hash(key)
-    puts "index for #{key} is #{index}"
     @bucket[index] = [key, value]
-    @size += 1
-    puts "#{@bucket}"
-  
   end
 
-  # takes one argument as a key and returns the value that is assigned to this key. If key is not found, return nil.
+  # returns the value of a given key. returns nil if key doesnt exist
   def get(key)
     index = hash(key)
     item = @bucket[index]
@@ -48,13 +44,12 @@ class HashMap
       val = item[1]
       puts "the value assigned to key '#{key}' is '#{val}'"
     else
+      val = nil
       puts "the value assigned to key '#{key}' is nil"
     end
-   
-
   end
 
-  # takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
+  # checks if the hashmap contains a given key
   def has?(key)
     index = hash(key)
     item = @bucket[index]
@@ -65,43 +60,78 @@ class HashMap
     end    
   end
 
-  # takes a key as an argument. If the given key is in the hash map, it should remove the entry with that key and return the deleted entry’s value. If the key isn’t in the hash map, it should return nil.
+  #removes the value from the key.
   def remove(key)
-    
+    index = hash(key)
+    item = @bucket[index]
+        if item[0] == key
+          item.delete_at(1)
+        end
+        puts "bucket = #{@bucket[index]}"
   end
 
   # returns the number of stored keys in the hash map.
   def length
-    
+    @bucket.each do |x| 
+      if x != [] then @size += 1
+      end
+    end
+        puts "the size of the hashmap is #{@size}"
+        load_factor = (@size.to_f / @bucket.length.to_f)
+        puts "the load factor of the hashmap is #{load_factor}"
   end
 
   # removes all entries in the hash map.
   def clear
-    
+    @bucket.each do |x| 
+      x == []
+    end
   end
 
   # returns an array containing all the keys inside the hash map.
   def keys
-
+    keys = []
+    @bucket.each do |x| 
+      if x != [] then keys.append(x[0])
+      end
+    end
+    puts "the list of keys in the hashmap is #{keys}"
   end
 
   # returns an array containing all the values.
   def values
-
+    values = []
+    @bucket.each do |x| 
+      if x != [] then values.append(x[1])
+      end
+    end
+    puts "the list of values in the hashmap is #{values}"
   end
 
   # returns an array that contains each key, value pair. Example: [[first_key, first_value], [second_key, second_value]]
   def entries
-    
+    entries = []
+    @bucket.each do |x| 
+      if x != [] then entries.append(x)
+      end
+    end
+    puts "the list of entries in the hashmap is #{entries}"
   end
 end
 
 test = HashMap.new
-test.set('pig', 'goat')
-
-test.set('pig', 'banana')
-test.get('pig')
-test.get('scrooge')
-
-test.has?('pig')
-test.has?('scrooge')
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+test.set('moon', 'silver')
+test.length
+test.entries
--- hashmap/main.rb
+++ hashmap/main.rb
@@ -15,4 +15,4 @@ test.set('jacket', 'blue')
 test.set('kite', 'pink')
 test.set('lion', 'golden')
 
-# test.set('moon', 'silver')
+test.set('moon', 'silver')
