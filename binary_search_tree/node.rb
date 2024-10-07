537bcbacbe240aaa75181a210116d6cc48fc77e2
megpeer <148973963+megpeer@users.noreply.github.com>
2024-10-07 09:03:50 -0400

    work commenced on tree build recursion. blind leading the blind

--- binary_search_tree/node.rb
+++ binary_search_tree/node.rb
@@ -1,15 +1,8 @@
 class Node
   attr_accessor :value, :left, :right
-  def initialize(value, left, right)
-    @value
+  def initialize(value)
+    @value = value
     @left
     @right
   end
-
-# def set_left()
-# end
- 
-#def set_right()
-#end
-
 end
--- binary_search_tree/tree.rb
+++ binary_search_tree/tree.rb
@@ -2,7 +2,7 @@ require_relative "node"
 
 class Tree
   attr_accessor :array, :root
-  def initialize(array)
+  def initialize
     @array = []
     @root = nil
   end
@@ -11,27 +11,33 @@ class Tree
     @array = array.sort.uniq
   end
 
-  def build_tree(array)
-    sort_array(array)
-    puts "lets build!"
-    p @array
-    # if (start>end) return nil
-    #   mid = (start + end/2)
-    #   @root = Node.new
-    #   root.set_left(array, start, mid - 1)
-    #   root.set_right(array, mid + 1, end)
-    #   return root
+  def build_tree_recur(array, start, last)
+    if (start > last) 
+      return nil
+    else
+      mid = (start + last / 2)
+      @root = Node.new(array[mid])
+      @root.left = build_tree_recur(array, start, mid - 1)
+      @root.right = build_tree_recur(array, mid + 1, last)
+      return @root
     end
   end
+  
 
-  # def pretty_print(node = @root, prefix = '', is_left = true)
-  #   pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-  #   puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-  #   pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
-  # end
+  def sorted_to_BST
+    return build_tree_recur(@array, 0, @array.length - 1)
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 
 
 end
 
 test = Tree.new
-test.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+test.sort_array([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+test.sorted_to_BST
+test.pretty_print
