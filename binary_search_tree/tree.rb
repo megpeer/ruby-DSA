434a9c446c738d5000d06e17fffbda2d22158743
megpeer <148973963+megpeer@users.noreply.github.com>
2024-10-14 13:50:36 -0400

    delete, find and level_order working

--- binary_search_tree/tree.rb
+++ binary_search_tree/tree.rb
@@ -43,17 +43,122 @@ class Tree
     end
   end
 
-#     Initilize the current node (say, currNode or node) with root node
-#     
-# Compare the key with the current node.
-# 
-# Move left if the key is less than or equal to the current node value.
-# 
-# Move right if the key is greater than current node value.
-# 
-# Repeat steps 2 and 3 until you reach a leaf node.
-# 
-# Attach the new key as a left or right child based on the comparison with the leaf node’s value.
+  # def delete(key, current = @root)
+  #   ##IF current node's key == input key
+  #   if current.key == key
+  #     #return if left and right are nil (leaf node)
+  #     return if current.left.nil? && current.right.nil?
+  #     #store the current node in a temp variable
+  #     temp = current
+  #       #until temp node has nil to the left, keep going left then return the right
+  #       if temp.left.nil?
+  #         temp = temp.right
+  #         return temp if temp.left.nil?
+  #         temp = temp.left until temp.left.nil?
+  #       else
+  #       #
+  #         temp = temp.left
+  #         return temp if temp.right.nil?
+  #         temp = temp.right until temp.right.nil?
+  #       end
+      
+  #     delete(temp.key, @root)
+  #     current.key = temp.key
+
+  #   ##IF root is bigger than key   
+  #   elsif current.key > key
+  #     current.left = delete(key, current.left)
+  #   ##IF root is less than key
+  #   else
+  #     current.right = delete(key, current.right)
+  #   end
+  # current
+  # end
+  
+  def get_successor(current)
+    current = current.right
+    while current != nil && current.left != nil
+      current = current.left
+    end
+    return current
+  end
+
+  def delete(key, current = @root)
+    #base case
+    if current == nil
+      return current
+    elsif current.key > key
+      current.left = delete(key, current.left)
+    elsif current.key < key
+      current.right = delete(key, current.right)
+    elsif current.left == nil 
+      return current.right
+    elsif current.right == nil
+      return current.left
+    end
+
+    #when both children are present
+      succ = get_successor(current)
+      current.key = succ.key
+      current.right = delete(succ.key, current.right)
+      current
+  end
+
+
+
+
+  def find(key)
+    current = @root
+    until current.nil?
+      return current if current.key == key
+      if current.key > key
+        current = current.left
+      elsif current.key < key
+        current = current.right
+      end
+    end
+  end
+
+  def level_order
+    arr = []
+    queue = [@root]
+    until queue.empty?
+        current = queue.shift
+        queue << current.left unless current.left == nil
+        queue << current.right unless current.right == nil
+        if block_given?
+          yield(current)
+        else
+        arr << current.key
+        end
+    end
+    p arr unless block_given?
+  end
+
+  def inorder(current = @root)
+      return if current.nil?
+        inorder(current.left)
+       p "#{current.key}" 
+        inorder(current.right)
+  end
+
+  def preorder(current = @root)
+    return if current.nil?
+    p "#{current.key}"
+    inorder(current.left)
+    inorder(current.right)
+  end
+
+  def postorder(current = @root)
+    return if current.nil?
+    
+    inorder(current.left)
+    inorder(current.right)
+    p "#{current.key}"
+    
+  end
+
+
 
   # do not adjsut below line
 #----------------------------------------------
@@ -65,6 +170,16 @@ class Tree
 end
 
 test = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+# # test.pretty_print
+# test.insert(11111)
+# test.insert (10)
 test.pretty_print
-test.insert(11111)
-test.pretty_print
+puts "level order traversal"
+test.level_order
+# test.level_order.each {|x| puts "#{x}"}
+puts "inorder traversal:"
+test.inorder
+puts "preorder traversal:"
+test.preorder
+puts "postorder traversal:"
+test.postorder
